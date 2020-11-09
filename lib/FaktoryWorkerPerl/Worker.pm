2de87624ae2d9e57667231acda8cc5c8982e7631
Kevin Murani <murani.kevin@gmail.com>
2020-11-09 18:29:45 +0100

    WIP: started working on adding support for concurrency

--- lib/FaktoryWorkerPerl/Worker.pm
+++ lib/FaktoryWorkerPerl/Worker.pm
@@ -51,6 +51,14 @@ has is_running => (
     default => sub { 0 },
 );
 
+has concurrency => (
+    is      => 'rw',
+    isa     => 'Int',
+    default => sub {
+        20;
+    },
+);
+
 use constant SLEEP_INTERVAL => 250_000;
 
 =over
@@ -68,7 +76,7 @@ sub register ( $self, $job_type, $callable ) {
     }
 
     unless ($callable) {
-        warn "A job processor cannot be undefined" unless $callable;
+        warn "A job processor cannot be undefined"           unless $callable;
         warn "A job processor must be a runnable subroutine" unless ref $callable eq 'CODE';
         return 0;
     }
@@ -96,34 +104,45 @@ sub run ( $self, $daemonize = 0 ) {
         last if ( $heartbeat eq 'terminate' );
         next if ( $heartbeat eq 'quiet' );
 
-        my $job = $self->client->fetch( $self->queues );
-        if ( $job && keys %$job ) {
-            eval {
-                my $callable = $self->job_types->{ $job->{jobtype} }
-                    or die sprintf( "No worker for job type: %s has been registered", $job->{jobtype} );
-
-                $callable->($job);
-                $self->client->ack( $job->{jid} );
-            } or do {
-                my $error = $@;
-                $self->logger->info( sprintf( "An error occured: %s for job: %s", $error, pp $job) );
-
-                my @backtrace = ();
-                my $i         = 1;
-                while ( ( my @caller_details = ( caller( $i++ ) ) ) ) {
-                    my ( $package, $filename, $line, $subroutine ) = @caller_details;
-                    push @backtrace, sprintf( "%s:%s in %s at %s", $package, $subroutine, $filename, $line );
-                }
-                $self->client->fail( $job->{jid}, "Exception", $error, [ reverse @backtrace ] );
-            };
-        } else {
-            $self->logger->info("no jobs to run at present");
-        }
+        # TODO: @amurani add thread logic here
+        $self->_process_job;
 
         $self->logger->info("worker has not been asked to stop") unless $self->stop;
 
         usleep( $self->SLEEP_INTERVAL );
     } while ( $daemonize && !$self->stop );
+}
+
+=item _process_job()
+
+Processes a single job from the faktory job server.
+
+=cut
+
+sub _process_job($self) {
+    my $job = $self->client->fetch( $self->queues );
+    if ( $job && keys %$job ) {
+        eval {
+            my $callable = $self->job_types->{ $job->{jobtype} }
+                or die sprintf( "No worker for job type: %s has been registered", $job->{jobtype} );
+
+            $callable->($job);
+            $self->client->ack( $job->{jid} );
+        } or do {
+            my $error = $@;
+            $self->logger->info( sprintf( "An error occured: %s for job: %s", $error, pp $job) );
+
+            my @backtrace = ();
+            my $i         = 1;
+            while ( ( my @caller_details = ( caller( $i++ ) ) ) ) {
+                my ( $package, $filename, $line, $subroutine ) = @caller_details;
+                push @backtrace, sprintf( "%s:%s in %s at %s", $package, $subroutine, $filename, $line );
+            }
+            $self->client->fail( $job->{jid}, "Exception", $error, [ reverse @backtrace ] );
+        };
+    } else {
+        $self->logger->info("no jobs to run at present");
+    }
 }
 
 __PACKAGE__->meta->make_immutable;
